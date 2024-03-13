997170d8d2a7ec056e635885414ff85ec2becf26
Raphael Palmer <palmer@Raphaels-Mac-Studio.local>
2024-03-13 20:32:14 -0300

    added scheduler to the index again

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from './Routes/routes';
 import dotenv from 'dotenv';
-import '/Services/scheduler';
+import '/Services/scheduler.ts';
 
 dotenv.config();
 const app = express();
