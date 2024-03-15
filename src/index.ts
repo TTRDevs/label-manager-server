8fb584b6daefd551609014ab2e980c94f92fdd02
Raphael Palmer <palmer@Raphaels-Mac-Studio.local>
2024-03-15 19:54:32 -0300

    reverted index to http

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,23 @@
-// import express from 'express';
-// import cors from 'cors';
-// import router from './Routes/routes';
-// import dotenv from 'dotenv';
-// import './Services/scheduler';
-
-// dotenv.config();
-// const app = express();
-// app.use(express.json());
-// app.use(cors({
-//   origin: process.env.CORS_ORIGIN?.split(',') || [
-//     'http://154.56.40.230:3000',
-//     'http://154.56.40.230:3001',
-//     'http://154.56.40.230:5432',
-//     'http://localhost:5173',
-//     'http://localhost:5432',
-//     'http://localhost:3001',
-//     'http://localhost:3000',
-//   ],
-//   credentials: true,
-// }));
-
-// app.use('/api', router);
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, welcome to the Label Manager Server!');
-// });
-
-// app.listen(3001, '0.0.0.0', () => {
-//   console.log('Server is up on port 3001');
-// });
-
 import express from 'express';
 import cors from 'cors';
 import router from './Routes/routes';
 import dotenv from 'dotenv';
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
+import './Services/scheduler';
 
 dotenv.config();
-const privateKeyPath = path.join('/etc/letsencrypt/live/recordlabelmanager.com', 'privkey.pem');
-const certificatePath = path.join('/etc/letsencrypt/live/recordlabelmanager.com', 'fullchain.pem');
-
-// Read the SSL certificate and private key
-const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
-const certificate = fs.readFileSync(certificatePath, 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
-
 const app = express();
 app.use(express.json());
 app.use(cors({
   origin: process.env.CORS_ORIGIN?.split(',') || [
-    'https://154.56.40.230:3000',
-    'https://154.56.40.230:3001',
-    'https://154.56.40.230:5432',
-    'https://localhost:5173',
-    'https://localhost:5432',
-    'https://localhost:3001',
-    'https://localhost:3000',
-    'https://metabase.recordlabelmanager.com',
-    'https://recordlabelmanager.com',
+    'http://154.56.40.230:3000',
+    'http://154.56.40.230:3001',
+    'http://154.56.40.230:5432',
+    'http://localhost:5173',
+    'http://localhost:5432',
+    'http://localhost:3001',
+    'http://localhost:3000',
   ],
-  credentials: false, // Typically, with cookies or auth you'd set this to true
+  credentials: true,
 }));
 
 app.use('/api', router);
@@ -71,8 +26,53 @@ app.get('/', (req, res) => {
   res.send('Hello, welcome to the Label Manager Server!');
 });
 
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(3001, () => {
-  console.log('HTTPS Server is up on port 3001');
+app.listen(3001, '0.0.0.0', () => {
+  console.log('Server is up on port 3001');
 });
+
+// import express from 'express';
+// import cors from 'cors';
+// import router from './Routes/routes';
+// import dotenv from 'dotenv';
+// import https from 'https';
+// import fs from 'fs';
+// import path from 'path';
+
+// dotenv.config();
+// const privateKeyPath = path.join('/etc/letsencrypt/live/recordlabelmanager.com', 'privkey.pem');
+// const certificatePath = path.join('/etc/letsencrypt/live/recordlabelmanager.com', 'fullchain.pem');
+
+// // Read the SSL certificate and private key
+// const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+// const certificate = fs.readFileSync(certificatePath, 'utf8');
+
+// const credentials = { key: privateKey, cert: certificate };
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN?.split(',') || [
+//     'https://154.56.40.230:3000',
+//     'https://154.56.40.230:3001',
+//     'https://154.56.40.230:5432',
+//     'https://localhost:5173',
+//     'https://localhost:5432',
+//     'https://localhost:3001',
+//     'https://localhost:3000',
+//     'https://metabase.recordlabelmanager.com',
+//     'https://recordlabelmanager.com',
+//   ],
+//   credentials: false, // Typically, with cookies or auth you'd set this to true
+// }));
+
+// app.use('/api', router);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, welcome to the Label Manager Server!');
+// });
+
+// const httpsServer = https.createServer(credentials, app);
+
+// httpsServer.listen(3001, () => {
+//   console.log('HTTPS Server is up on port 3001');
+// });
