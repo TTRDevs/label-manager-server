495c716fd24ec78d82b2361493adc1d3635c9400
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-03-14 22:22:52 -0300

    included SSL certificate and replaced http for https

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,33 @@ import express from 'express';
 import cors from 'cors';
 import router from './Routes/routes';
 import dotenv from 'dotenv';
-import './Services/scheduler';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
+const privateKeyPath = path.join('/etc/letsencrypt/live/recordlabelmanager.com', 'privkey.pem');
+const certificatePath = path.join('/etc/letsencrypt/live/recordlabelmanager.com', 'fullchain.pem');
+
+// Read the SSL certificate and private key
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
 const app = express();
 app.use(express.json());
 app.use(cors({
   origin: process.env.CORS_ORIGIN?.split(',') || [
-    'http://154.56.40.230:3000',
-    'http://154.56.40.230:3001',
-    'http://154.56.40.230:5432',
-    'http://localhost:5173',
-    'http://localhost:5432',
-    'http://localhost:3001',
-    'http://localhost:3000',
+    'https://154.56.40.230:3000',
+    'https://154.56.40.230:3001',
+    'https://154.56.40.230:5432',
+    'https://localhost:5173',
+    'https://localhost:5432',
+    'https://localhost:3001',
+    'https://localhost:3000',
   ],
-  credentials: false,
+  credentials: false, // Typically, with cookies or auth you'd set this to true
 }));
 
 app.use('/api', router);
@@ -26,6 +37,8 @@ app.get('/', (req, res) => {
   res.send('Hello, welcome to the Label Manager Server!');
 });
 
-app.listen(3001, '0.0.0.0', () => {
-  console.log('Server is up on port 3001');
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3001, () => {
+  console.log('HTTPS Server is up on port 3001');
 });
